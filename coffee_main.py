c7b7667f0858bf2502157e2afd056cad230e6f59
thekaranacharya <Nanospeed19>
2020-05-13 13:31:27 +0530

    Add a loop add exit and remaining

--- coffee_main.py
+++ coffee_main.py
@@ -21,20 +21,25 @@ cappu_beans = 12  # g
 cappu_cost = 6  # $
 
 
-def print_available_supplies():
+def show_available_supplies() :
     available_supplies = """
         The coffee machine has:
         {} of water
         {} of milk
         {} of coffee beans
         {} of disposable cups
-        {} of money
-        """.format(water_available, milk_available, coffee_beans_available, disposable_cups_available, money_available)
+        ${} of money
+        """.format(water_available,
+                   milk_available,
+                   coffee_beans_available,
+                   disposable_cups_available,
+                   money_available)
 
     print(available_supplies)
 
 
 def is_coffee_available(coffee_index):
+
     if coffee_index == 1:
         water_per_cup = espresso_water
         milk_per_cup = espresso_milk
@@ -47,38 +52,44 @@ def is_coffee_available(coffee_index):
         water_per_cup = cappu_water
         milk_per_cup = cappu_milk
         beans_per_cup = cappu_beans
-    else:
-        print('Invalid index entered, please try again!')
 
-    cups_with_milk = 1000  # large number
 
     cups_with_water = water_available // water_per_cup
+    cups_with_milk = 1000  # large number
 
     if milk_per_cup != 0:
         cups_with_milk = milk_available // milk_per_cup
 
     cups_with_beans = coffee_beans_available // beans_per_cup
+    min_cups_possible = min(cups_with_water, cups_with_milk, cups_with_beans)
 
-    cups_possible = min(cups_with_water, cups_with_milk, cups_with_beans)
-    if cups_possible > 0:
+    if min_cups_possible > 0:
         return True
     else:
-        return False
+        # if min_cups_possible = 0 i.e. either of the ingredients is 0
+        ingredients = [cups_with_water, cups_with_milk, cups_with_beans]
+        return ingredients.index(min(ingredients))
 
 
 def buy():
-    print("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:")
-    coffee_index = int(input().strip())
+    print("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu:")
+    coffee_index = input().strip()
 
-    if is_coffee_available(coffee_index):
-        global water_available, milk_available, coffee_beans_available, disposable_cups_available, money_available
+    if coffee_index == 'back':
+        return
 
-        # Check if there are disposable_cups_available
-        if disposable_cups_available > 0:
-            # reducing the quantity of available disposable cups by 1
+    global water_available, milk_available, coffee_beans_available, disposable_cups_available, money_available
+
+    if disposable_cups_available > 0:
+
+        prep_status = is_coffee_available(int(coffee_index))
+
+        if prep_status:  # if True
+
+            print('I have enough resources, making you a coffee!')
             disposable_cups_available -= 1
 
-            if coffee_index == 1:
+            if coffee_index == '1':
                 # espresso
                 # subtracting the quantity of ingredients
                 water_available -= espresso_water
@@ -87,7 +98,7 @@ def buy():
                 # adding the cost to the money
                 money_available += espresso_cost
 
-            elif coffee_index == 2:
+            elif coffee_index == '2':
                 # latte
                 # subtracting the quantity of ingredients
                 water_available -= latte_water
@@ -96,7 +107,7 @@ def buy():
                 # adding the cost to the money
                 money_available += latte_cost
 
-            elif coffee_index == 3:
+            elif coffee_index == '3':
                 # cappuchino
                 # subtracting the quantity of ingredients
                 water_available -= cappu_water
@@ -106,13 +117,14 @@ def buy():
                 money_available += cappu_cost
 
         else:
-            print("Sorry, we don't have any disposable cups to pour coffee in. Please try again later!")
+            ingredients_list = ['water', 'milk', 'coffee beans']
+            print('Sorry, not enough {}!'.format(ingredients_list[prep_status]))
 
-    else:
-        print("Sorry, your desired coffee is not available. Please try again later!")
+    else :
+        print('Sorry, not enough disposable cups!')
 
 
-def fill():
+def fill() :
     global water_available, milk_available, coffee_beans_available, disposable_cups_available
 
     print("Write how many ml of water do you want to add:")
@@ -132,27 +144,24 @@ def fill():
     disposable_cups_available += add_cups
 
 
-def take():
+def take() :
     global money_available
     print("I gave you ${}".format(money_available))
     money_available = 0
 
 
 #   ----------------------- main program execution starts here -------------------------
-print_available_supplies()
 
-print("Write action (buy, fill, take):")
-action = input().strip()
-if action == 'buy':
-    buy()
-elif action == 'fill':
-    fill()
-elif action == 'take':
-    take()
-else:
-    print("Invalid action entered, please try again!")
-
-print_available_supplies()
-
-
-
+while True:
+    print("Write action (buy, fill, take, remaining, exit):")
+    action = input().strip()
+    if action == 'buy':
+        buy()
+    elif action == 'fill':
+        fill()
+    elif action == 'take':
+        take()
+    elif action == 'remaining':
+        show_available_supplies()
+    elif action == 'exit':
+        break
